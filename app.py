f3911deb0afd1c48c8e9ddaa5fe8e86a20a44497
unknown <maiju.nurmi@myy.haaga-helia.fi>
2019-04-28 20:36:45 +0300

    get json from fronntend

--- app.py
+++ app.py
@@ -44,38 +44,44 @@ CORS(app)
 
 @app.route('/getemissions', methods=['POST', 'GET'])
 def getData():
-     with open('./paastokansio/paastot.csv','r') as paastot:
-             readCSV = csv.reader(paastot, delimiter=',')
-             #country = input("from what country do you want info? ")
-             #inputyear = input("For what year do you want info? ")
-             country = 'Finland'
-             inputyear = '1961'
-             headers=[]
-             countries=[]
-             data=[]
-             #loop trough rows in file
-             for row in readCSV:
-                 numOfLines = readCSV.line_num
-                 #We dont need the fist 4 lines
-                 #Line 5 is headers. We can find
-                 if numOfLines == 5:
-                     headers = row
-                     print(headers)
-                     indexY = headers.index(inputyear)
-                     year = row[indexY]
-                     print(year)
-                     data.append(year)
-                 elif numOfLines > 5:
-                     #add all countruNames to list
-                    countries.append(row[0])
-                    #find selected country and co2emissions from selected year
-                    if row[0] == country :
-                        chosenCountry = row[0]
-                        data.append(chosenCountry)
-                        print(country + " CO2 emissions in the year: " + inputyear)
-                        emissions = row[indexY]
-                        data.append(emissions)
-                        return jsonify({'data': data})
+        d = request.form.to_dict()
+        print(d)
+        post_data = request.get_json()
+        country  = post_data['formData']['country']
+        inputyear  = post_data['formData']['year']
+        print(country )
+        with open('./paastokansio/paastot.csv','r') as paastot:
+                 readCSV = csv.reader(paastot, delimiter=',')
+                 #country = input("from what country do you want info? ")
+                 #inputyear = input("For what year do you want info? ")
+                 #country = 'Finland'
+                 #inputyear = '1961'
+                 headers=[]
+                 countries=[]
+                 data=[]
+                 #loop trough rows in file
+                 for row in readCSV:
+                     numOfLines = readCSV.line_num
+                     #We dont need the fist 4 lines
+                     #Line 5 is headers. We can find
+                     if numOfLines == 5:
+                         headers = row
+                         #print(headers)
+                         indexY = headers.index(inputyear)
+                         year = row[indexY]
+                         print(year)
+                         data.append(year)
+                     elif numOfLines > 5:
+                         #add all countruNames to list
+                        countries.append(row[0])
+                        #find selected country and co2emissions from selected year
+                        if row[0] == country :
+                            chosenCountry = row[0]
+                            data.append(chosenCountry)
+                            print(country + " CO2 emissions in the year: " + inputyear)
+                            emissions = row[indexY]
+                            data.append(emissions)
+                            return jsonify({'data': data})
 
 if __name__ == '__main__':
     app.run()
